c17d0480e83093aeebca039d7c67f9278e73d3bd
pbelouin <pbelouin@icloud.com>
2016-10-11 21:14:35 +0200

    procfile

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,12 +9,18 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
 config :phoenix_api, PhoenixApi.Endpoint,
-  secret_key_base: "rCw4N1JQnS4BJfMYODujRoxOn5vjTiy0UCvlKhk7Ea5xmdM8M8JcQGAR0ULGfPlI"
+  secret_key_base: System.get_env("SECRET_KEY")
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "myphoenixapi.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
 
 # Configure your database
 config :phoenix_api, PhoenixApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "phoenix_api_prod",
-  pool_size: 20
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+# Do not print debug messages in production
+config :logger, level: :info
